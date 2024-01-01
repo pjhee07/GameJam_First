84e42966b482aafcca7e0c31870381d2ef32347d
김수현 <sikhye0u0@gmail.com>
2024-01-02 01:50:37 +0900

    0102

---
+++ Assets/4/02 1.Scripts/PlayerAnim.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnim : MonoBehaviour
+{
+    Animator anim;
+    PlayerCtrl playerCtrl;
+
+
+    private void Awake()
+    {
+        playerCtrl = GetComponent<PlayerCtrl>(); //¡÷¿«
+    }
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        playerCtrl.onRunChanged += onRun;
+        playerCtrl.onJumpChanged += onJump;
+    }
+
+    void onRun(float hor)
+    {
+        anim.SetFloat("Hor", hor);
+    }
+
+    void onJump(bool isJump)
+    {
+        anim.SetBool("Jump", isJump);
+    }
+
+
+}
