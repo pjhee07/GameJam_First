3f1b689bbf89f035abe74c4a42899b9caef233fe
yoonjunhee <123710778+yoonjunhee@users.noreply.github.com>
2024-01-02 23:42:38 +0900

    레버 수정

--- Assets/15/Script_y/Door/Leber2.cs
+++ Assets/15/Script_y/Door/Leber2.cs
@@ -2,35 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Leber : MonoBehaviour
+public class Leber2 : MonoBehaviour
 {
     [SerializeField] private GameObject EImage;
     public GameObject dor2;
-   
-    Door door;
+    
     Animator animator;
     private void Start()
     {
         animator = GetComponent<Animator>();
-        door = GetComponentInChildren<Door>();
-        
+       
+
     }
     private void Update()
     {
-        if(EImage.activeSelf&&Input.GetKeyDown(KeyCode.E))
+        if (EImage.activeSelf && Input.GetKeyDown(KeyCode.E))
         {
             animator.SetTrigger("On");
-            door.enabled = true;
-           
+          
+            dor2.GetComponent<Door>().enabled = true;
         }
     }
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Player"))
+        if (collision.CompareTag("Player"))
         {
-            
+           
             EImage.SetActive(true);
         }
     }
