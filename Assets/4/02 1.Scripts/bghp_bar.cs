ea192ac0ecbe1dc454f804be8e48f376fecd00ad
김수현 <sikhye0u0@gmail.com>
2024-01-03 14:51:20 +0900

    .

---
+++ Assets/4/02 1.Scripts/bghp_bar.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bghp_bar : Singleton<bghp_bar>
+{
+
+    protected override void Awake()
+    {
+        base.Awake();
+        
+    }
+
+}
