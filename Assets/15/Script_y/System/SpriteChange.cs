649a297d226bdcb4326350c937b016214c60f665
yoonjunhee <123710778+yoonjunhee@users.noreply.github.com>
2024-01-04 16:09:34 +0900

    수정

--- Assets/15/Script_y/System/SpriteChange.cs
+++ Assets/15/Script_y/System/SpriteChange.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpriteChange : MonoBehaviour
 {
 
     SpriteRenderer spriteRenderer;
     [SerializeField] private string NextScene;
-
+    [SerializeField] private Image FadeImage;
     [SerializeField] private Sprite[] sprite;
     private void Awake()
     {
@@ -15,20 +16,34 @@ public class SpriteChange : MonoBehaviour
     }
     private void Start()
     {
+        FadeImage.enabled = false;
         StartCoroutine(ChangeSpriteRotine());
         
     }
 
     IEnumerator ChangeSpriteRotine()
     {
-        yield return new WaitForSeconds(4f);
+        yield return new WaitForSeconds(4.5f);
         spriteRenderer.sprite = sprite[0];
         yield return new WaitForSeconds(3f);
         spriteRenderer.sprite = sprite[1];
         yield return new WaitForSeconds(4.5f);
         spriteRenderer.sprite = sprite[2];
-        yield return new WaitForSeconds(3f);
-        GameManager.Instance.SceneMovement(NextScene);
+        yield return new WaitForSeconds(2f);
+        StartCoroutine(FadeRotine());
         Debug.Log("Á¾·á");        
+    }
+
+    IEnumerator FadeRotine()
+    {
+        FadeImage.enabled = true;
+        float currentime = 0;
+        while(currentime<1)
+        {
+            currentime += Time.deltaTime;
+            FadeImage.color = new Color(1, 1, 1, currentime);
+            yield return new WaitForSeconds(0.01f);
+        }
+        GameManager.Instance.SceneMovement(NextScene);
     }
 }
