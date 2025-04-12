3da33542ae7ef7ebcac8b70a1ba20c9bd97d5573
김식혜 <139305586+GimSikhye@users.noreply.github.com>
2025-04-12 17:52:52 +0900

    폴더 대강 정리
    왜 이따구로 정리해놨지 하하

---
+++ Assets/02.Scripts/Button/BtnPush.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BtnPush : MonoBehaviour
+{
+    Animator anim;
+    BoxCollider2D box;
+    [SerializeField] private GameObject brigde;
+    Brigde brigde2;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        box = GetComponent<BoxCollider2D>();
+        brigde2 = GetComponentInParent<Brigde>();
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Object"))
+        {
+            anim.SetTrigger("Push");
+            brigde2.PusCountSet();
+            box.enabled = false;
+            if (brigde2.PushFlage == true)
+            {
+                brigde.SetActive(true);
+                SoundManager.Instance.PlaySound(SoundManager.Sound.Brigde);
+            }
+        }
+    }
+}
